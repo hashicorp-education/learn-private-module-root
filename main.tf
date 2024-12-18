4c691b50a593881d13b357d89b02aa964a357b3f
Lee Forbes <fillthatinfo@gmail.com>
2024-12-18 12:45:22 -0600

    updated permissions for terraform_user

--- main.tf
+++ main.tf
@@ -9,3 +9,34 @@ module "s3-webapp" {
   prefix  = var.prefix
   version = "1.0.0"
 }
+
+resource "aws_s3_bucket_acl" "frontend_acl" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+  acl    = "public-read"
+
+depends_on = [
+  aws_s3_bucket_public_access_block.frontend_public_access_block,
+]
+}
+
+resource "aws_s3_bucket_policy" "frontend_bucket_policy" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::${aws_s3_bucket.frontend_bucket.id}/*"
+    }
+  ]
+}
+POLICY
+
+depends_on = [
+  aws_s3_bucket_public_access_block.frontend_public_access_block,
+]
+}
